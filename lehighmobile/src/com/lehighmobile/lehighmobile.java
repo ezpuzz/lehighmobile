24669402c5919588cb0da488b1713020fa6dbd17
invaderspleen <invaderspleen@4bc1a39c-69d5-1e4e-e592-e7c7b0be5d0a>
2010-10-13 03:21:08 +0000

    get directions now displays inactive map screen

--- lehighmobile/src/com/lehighmobile/lehighmobile.java
+++ lehighmobile/src/com/lehighmobile/lehighmobile.java
@@ -1,24 +1,38 @@
 package com.lehighmobile;
 
-import android.app.Activity;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 
-public class lehighmobile extends Activity{
+public class lehighmobile extends MapActivity{
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        Button btn = (Button)findViewById(R.id.button);
+        Button btn = (Button)findViewById(R.id.button1);
         btn.setOnClickListener(new View.OnClickListener(){
         	public void onClick(View v){
-        		
+        		setContentView(R.layout.map);
+        		LinearLayout linearLayout;
+                MapView mapView;
+                
+                mapView = (MapView)findViewById(R.id.mapview);
+                mapView.setBuiltInZoomControls(true);
         	}
         });
-
+        
+        
     }
-   
+    
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
+    
 }
