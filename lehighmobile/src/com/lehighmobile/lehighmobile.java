423801514f7c3f5ebfe1b402e6cf4f277e8e482f
WyattPCA2@gmail.com <WyattPCA2@gmail.com@4bc1a39c-69d5-1e4e-e592-e7c7b0be5d0a>
2010-11-13 19:20:12 +0000

    Commit at 2:17pm on 11/13/2010 by Wyatt This has an array set up so that the coordinates are an easy to use variable

--- lehighmobile/src/com/lehighmobile/lehighmobile.java
+++ lehighmobile/src/com/lehighmobile/lehighmobile.java
@@ -1,5 +1,6 @@
 package com.lehighmobile;
 
+import java.util.*;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
 
@@ -14,7 +15,8 @@ import android.widget.Toast;
 
 public class lehighmobile extends MapActivity {
 	static final int DIALOG_BUILDING_LIST = 0;
-
+	/*currently lookup holds LL, then PA*/
+	static final String [] lookup = {"40.607425, -75.374926", "40.607857,-75.378995"};
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -31,18 +33,29 @@ public class lehighmobile extends MapActivity {
 
 	protected Dialog onCreateDialog(int id){
 	Dialog d = null;
+	/*HashMap <String, String>hm = new HashMap();
+	hm.put("LL", "40.607425, -75.374926");
+	hm.put("Packard   PA", "40.607857,-75.378995");*/
 	switch(id){
 	case DIALOG_BUILDING_LIST:
-		final CharSequence[] items = {"Lewis Lab", "Packard"};
+		final CharSequence[] items = {"Lewis Lab	LL", "Packard	PA"};
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		builder.setTitle("Choose Destination");
 		builder.setItems(items, new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog, int item) {
-				 setContentView(R.layout.map);
-				 MapView mapView;
+				String coordinates = lookup[item];
 				
-				 mapView = (MapView)findViewById(R.id.mapview);
-				 mapView.setBuiltInZoomControls(true);
+				/*variable coordinates will hold the coordinates as a string */
+				if (item < 2){
+					setContentView(R.layout.map);
+					MapView mapView;
+				
+					mapView = (MapView)findViewById(R.id.mapview);
+					mapView.setBuiltInZoomControls(true);
+				}
+				else{
+					/*This means somthing went wrong, or item is not what I think it is*/
+				}
 			}
 		});
 		AlertDialog alert = builder.create();
