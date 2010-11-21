5f02f7d46140e10e2a60f810ef532e200c0b6498
invaderspleen <invaderspleen@4bc1a39c-69d5-1e4e-e592-e7c7b0be5d0a>
2010-11-21 19:55:34 +0000

    map works now

--- trunk/lehighmobile/src/com/lehighmobile/lehighmobile.java
+++ trunk/lehighmobile/src/com/lehighmobile/lehighmobile.java
@@ -1,6 +1,8 @@
 package com.lehighmobile;
 
-import java.util.*;
+import java.util.List;
+
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
 
@@ -10,13 +12,12 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
 public class lehighmobile extends MapActivity {
 	static final int DIALOG_BUILDING_LIST = 0;
-	/*currently lookup holds LL, then PA*/
-	static final String [] lookup = {"40.607425, -75.374926", "40.607857,-75.378995"};
+	/* currently lookup holds LL, then PA */
+	static final GeoPoint[] locationCoordinates = { new GeoPoint(40607425, -75374926), new GeoPoint(40607857,-75378995) };
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -26,45 +27,45 @@ public class lehighmobile extends MapActivity {
 		btn.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				showDialog(DIALOG_BUILDING_LIST);
-				
+
 			}
 		});
 	}
 
-	protected Dialog onCreateDialog(int id){
-	Dialog d = null;
-	/*HashMap <String, String>hm = new HashMap();
-	hm.put("LL", "40.607425, -75.374926");
-	hm.put("Packard   PA", "40.607857,-75.378995");*/
-	switch(id){
-	case DIALOG_BUILDING_LIST:
-		final CharSequence[] items = {"Lewis Lab	LL", "Packard	PA"};
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setTitle("Choose Destination");
-		builder.setItems(items, new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int item) {
-				String coordinates = lookup[item];
-				
-				/*variable coordinates will hold the coordinates as a string */
-				if (item < 2){
-					setContentView(R.layout.map);
-					MapView mapView;
-				
-					mapView = (MapView)findViewById(R.id.mapview);
-					mapView.setBuiltInZoomControls(true);
-				}
-				else{
-					/*This means somthing went wrong, or item is not what I think it is*/
-				}
-			}
-		});
-		AlertDialog alert = builder.create();
-		d = alert;
-		break;
-	}
-	return d;
+	protected Dialog onCreateDialog(int id) {
+		Dialog d = null;
+		switch (id) {
+		case DIALOG_BUILDING_LIST:
+			final CharSequence[] items = { "Lewis Lab	LL", "Packard	PA" };
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("Choose Destination");
+			builder.setItems(items, new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int item) {
 
-}
+					if (item < 2) {
+						setContentView(R.layout.map);
+						MapView mapView;
+
+						mapView = (MapView) findViewById(R.id.mapview);
+						mapView.setBuiltInZoomControls(true);
+						
+						mapView.getController().setZoom(18);
+						mapView.getController().setCenter(locationCoordinates[item]);
+					} else {
+						/*
+						 * This means somthing went wrong, or item is not what I
+						 * think it is
+						 */
+					}
+				}
+			});
+			AlertDialog alert = builder.create();
+			d = alert;
+			break;
+		}
+		return d;
+
+	}
 
 	@Override
 	protected boolean isRouteDisplayed() {
